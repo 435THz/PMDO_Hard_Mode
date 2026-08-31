b31073f31774d7fd8cce302fe779c9006eddaaba
435THz <delbovemarie@gmail.com>
2026-08-31 10:50:18 +0200

    Only process foes

--- Data/Script/pmdo_hard_mode/event_single.lua
+++ Data/Script/pmdo_hard_mode/event_single.lua
@@ -2,19 +2,15 @@ require 'origin.common'
 
 function SINGLE_CHAR_SCRIPT.HardModeProcessCharacter(owner, ownerChar, context, args)
 	if not context.User then return end
-	if context.User.MemberTeam.MapFaction == RogueEssence.Dungeon.Faction.Player then
-		if context.User.LuaData.HARD_MODE then
-            context.User.ProxyAtk = -1;
-            context.User.ProxyDef = -1;
-            context.User.ProxyMAtk = -1;
-            context.User.ProxyMDef = -1;
-            context.User.ProxySpeed = -1;
-			context.User.LuaData.HARD_MODE = nil
-		end
-	else
-		if not context.User.LuaData.HARD_MODE then
-			HARD_MODE.applySpawnEffects(context.User)
-			context.User.LuaData.HARD_MODE = true
-		end
+	if context.User.MemberTeam.MapFaction == RogueEssence.Dungeon.Faction.Player and context.User.LuaData.HARD_MODE then
+        context.User.ProxyAtk = -1;
+        context.User.ProxyDef = -1;
+        context.User.ProxyMAtk = -1;
+        context.User.ProxyMDef = -1;
+        context.User.ProxySpeed = -1;
+		context.User.LuaData.HARD_MODE = nil
+	elseif context.User.MemberTeam.MapFaction == RogueEssence.Dungeon.Faction.Foe and not context.User.LuaData.HARD_MODE then
+		HARD_MODE.applySpawnEffects(context.User)
+		context.User.LuaData.HARD_MODE = true
 	end
 end
